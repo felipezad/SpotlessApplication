e97954c19c62e7a69ac2f345c119257d0a556ec3
felipe.zadinello <felipe.zadinello@blacklane.com>
2024-08-01 15:46:13 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.spotless) apply true
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        target("**/src/**/*.kt")
+        targetExclude("**/build/**/*.kt", "**/generated/**/*.kt")
+        ktlint("1.3.1")
+            .setEditorConfigPath("$projectDir/.editorconfig")
+    }
+    format("kts") {
+        target("**/src/**/*.kts")
+        targetExclude("**/build/**/*.kts", "**/generated/**/*.kts")
+    }
+}
